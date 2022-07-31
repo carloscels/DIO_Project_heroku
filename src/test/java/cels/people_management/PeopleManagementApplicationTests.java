6cf278dc51ba8106171ad43f78c24088c9e4829b
caducels <carlos.cels.1999@gmail.com>
2022-07-31 08:25:43 -0300

    apricação heroku com java

---
+++ src/test/java/cels/people_management/PeopleManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package cels.people_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopleManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
